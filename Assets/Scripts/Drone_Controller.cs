d30e785f96647954660c7d544949fc38c551043e
patriciochavez <patricio_chavez@hotmail.com>
2016-12-25 21:00:51 -0300

    first commit

---
+++ Assets/Scripts/Drone_Controller.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Drone_Controller : MonoBehaviour {
+    public float speed;
+    public float turn_speed;
+
+    // Use this for initialization
+    void Start () {
+        speed = 10f;
+        turn_speed = 10f;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            transform.Translate(Vector3.up * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector3.down * Time.deltaTime * speed);
+        }
+        if (Input.GetButton("Fire1"))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.up, -turn_speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(Vector3.up, turn_speed * Time.deltaTime);
+        }
+    }
+}
